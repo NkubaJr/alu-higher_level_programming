b3f5da683c33cc3c669d8cc74445f0ccd89db9ae
NkubaJr <j.igiraneza1@alustudent.com>
2025-05-31 07:49:48 -0700

    JS

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
+const argsCount = process.argv.length;
+const numbers = [];
+switch (argsCount) {
+	  case 2:
+		  case 3:
+		    console.log(0);
+		    break;
 
-if (process.argv.length <= 3) {
-	  console.log('0')
-} else {
-	  const arr = process.argv.slice(2).map(Number)
-	  const second = arr.sort((a, b) => b - a)[1]
-	  console.log(second)
+		  default:
+		    for (let i = 2; i < argsCount; i++) {
+			          numbers.push(process.argv[i]);
+			        }
+		    numbers.sort((a, b) => b - a);
+		    console.log(numbers[1]);
+		    break;
 }
-
