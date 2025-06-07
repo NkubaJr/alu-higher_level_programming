f37d5462ea337fcb6147cd88bc10c7344bbfee57
NkubaJr <j.igiraneza1@alustudent.com>
2025-06-07 05:22:16 -0700

    JS

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-const fs = require('fs');
+const fs = require('fs')
+
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
-	  if (err) console.log(err);
-});
+	  if (err) console.log(err)
+})
 
