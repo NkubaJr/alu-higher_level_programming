d1d2799bea236b6e140a72d89fda990d6c9008d3
NkubaJr <j.igiraneza1@alustudent.com>
2025-06-07 05:18:07 -0700

    JS

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
-const fs = require('fs');
 
-fs.readFile(process.argv[2], (err, contents) => {
-	  err ? console.log(err) : console.log(contents.toString());
+const fs = require('fs');
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', function (err, data) {
+	  if (err) {
+		      console.log(err);
+		    } else {
+			        console.log(data);
+			      }
 });
+
